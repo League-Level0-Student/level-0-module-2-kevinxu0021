f667afc476444e35b8dcc05332800989187c534a
Garrett-Thomas <league@WTS5.attlocal.net>
2017-11-22 11:30:08 -0800

    Random Package

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -8,23 +8,21 @@ import java.util.Random;
 //1. Run the program.  What number appears in the console?
 //   Run it again. Is the number the same?
 
-
 public class IntroToRandom {
 	public static void main(String[] args) {
-		int r = 0;
-		
-		//2. Now make r random. 
-		//   Create an object of the Random class
-		//   Hint: new Random().nextInt()
+		Random rand = new Random();
 
-		//3. Limit the random number between 0 and 100
+		// 2. Now make r random.
+		// Create an object of the Random class
+		// Hint: new Random().nextInt()
+		int r = rand.nextInt(88 - -222) + -222;
+		// 3. Limit the random number between 0 and 100
 
-		//4. Limit the random number between 25 and 75. Hint: (highest value - lowest value) + lowest value
+		// 4. Limit the random number between 25 and 75. Hint: (highest value - lowest
+		// value) + lowest value
 
-		//5. Challenge: Limit the random number between -222 and 88
+		// 5. Challenge: Limit the random number between -222 and 88
 
-
-		
-		System.out.println(r); 
+		System.out.println(r);
 	}
 }
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -10,18 +10,30 @@ import javax.swing.JOptionPane;
 public class Validation {
 	public static void main(String[] args) {
 
-		// 1. OPTIONAL: Watch the first 2 minutes of this movie: http://vimeo.com/2485018
-		
+		// 1. OPTIONAL: Watch the first 2 minutes of this movie:
+		// http://vimeo.com/2485018
+
 		int randomNumber = new Random().nextInt(5);
-		
-		// 2. On paper, write all the numbers that get printed when you run this class 10 times
-		
+
+		// 2. On paper, write all the numbers that get printed when you run this class
+		// 10 times
+
 		System.out.println(randomNumber);
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
-
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "You look good");
+		} else if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "Your brethe smells good");
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "Your smile is the best");
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "your face is good today");
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "Your job is hard but it is good");
+		}
 		// 4. Repeat all the code above 10 times
-		
+
 		// 5. Find someone to test out your program. They will like it :)
 	}
 }
