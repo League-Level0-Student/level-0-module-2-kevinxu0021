3f6cc02b9293055de7cb63817ef53c2e8c98ae14
kevinxu0021 <league@WTS19.attlocal.net>
2017-12-06 19:27:50 -0800

    What I did on Dec 6, 2017
    That Was Easy, Nasa Countdown, and Star Show

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -3,7 +3,9 @@
 
 package extra;
 
- /* NASA wants to shoot a rocket into orbit. 
+import javax.swing.JOptionPane;
+
+/* NASA wants to shoot a rocket into orbit. 
   * Somebody has to do the countdown – or the rocket won’t launch.
  * 
  * The ground control chief will tell you where to start – then count down to 0. 
@@ -15,22 +17,36 @@ package extra;
 public class NasaCountdown {
 	public static void main(String[] args) {
 		// 2. Use a pop-up to ask the user where to start counting from
-
+String answer = JOptionPane.showInputDialog("Where do you want to start counting from?");
+int number = Integer.parseInt(answer);
 		// 3. Change the countdown to use the new starting point
 		
 		// 1. Print a countdown from 10 to 0 on the console
-
+for (int i = number; i >= 0; i--) {
+System.out.println(i);
 		// 4. Use the speak method to hear the countdown.
-		
+		speak(i);
+
 		// 5. when the counting is done, speak "blastoff!"
+if(i == 0) {
+	speak("blastoff!");
+}
+}
 	}
 
-static void speak(String words) {
+static void speak(int words) {
    	 try {
    		 Runtime.getRuntime().exec("say " + words).waitFor();
    	 } catch (Exception e) {
    		 e.printStackTrace();
    	 }
+   	 }
+   	static void speak(String words) {
+      	 try {
+      		 Runtime.getRuntime().exec("say " + words).waitFor();
+      	 } catch (Exception e) {
+      		 e.printStackTrace();
+      	 }
   }
 }
 
