708eabda61c8e2e54dc293ac2115e5410176fde6
not you <league@WTS5.attlocal.net>
2017-12-09 12:57:54 -0800

    What I did on December 9,2017

--- src/extra/StringMethods1.java
+++ src/extra/StringMethods1.java
@@ -2,30 +2,31 @@
 //    Level 0
 package extra;
 
-
 public class StringMethods1 {
 	public static void main(String[] args) {
-		
+
 		// 1. Create a String variable and initialize it to whatever you want
-
+		String Hi = "what are you doing?";
 		// 2. Print the 3rd char of your String to the console.
-		//    HINT: .charAt
-		
+		// HINT: .charAt
+		System.out.println(Hi.charAt(3));
 		// 3. Print the length of your String to the console.
-		//    HINT: .length
-
+		// HINT: .length
+		System.out.println(Hi.length());
 		// 4. Using a for loop, print one char at time to the console.
-		//    HINT: use .length() to determine how many loops
-
-		
+		// HINT: use .length() to determine how many loops
+		for (int i = 0; i < Hi.length(); i++) {
+			System.out.println(Hi.charAt(i));
+		}
 		// 5. Pick a char inside your String, and use a loop to determine
-		//    what position/index in the String the char is located.
-		//    Print the char's position to the console.
-		//    EXAMPLE: if your string is "abc" and you are searching 
-		//             for char 'b', then print "b is at index 1"
-		
+		// what position/index in the String the char is located.
+		// Print the char's position to the console.
+		// EXAMPLE: if your string is "abc" and you are searching
+		// for char 'b', then print "b is at index 1"
+		for (int i = 0; i < Hi.length(); i++) {
+			if (Hi.charAt(i) == 'a') {
+				System.out.println("\"a\" is at " + i);
+			}
+		}
 	}
 }
-
-
-
--- src/extra/StringMethods2.java
+++ src/extra/StringMethods2.java
@@ -4,22 +4,24 @@ package extra;
 
 public class StringMethods2 {
 	public static void main(String[] args) {
-		
+
 		// 1. Create a String variable and initialize it with
-		//    random upper and lower case characters.
-
+		// random upper and lower case characters.
+		String C = "AbCdE";
 		// 2. Print your String to the console in upper case.
-		
+		System.out.println(C.toUpperCase());
 		// 3. Print your String to the console in lower case.
-
+		System.out.println(C.toLowerCase());
 		// 4. Print the first 3 char's of your String
-		//    HINT: .substring(start,end)
-		
+		// HINT: .substring(start,end)
+		System.out.println(C.substring(0, 3));
 		// 5. Print a single char somewhere in the middle of your String
-		//    REMINDER: char's in string start at index 0
-		
+		// REMINDER: char's in string start at index 0
+		System.out.println(C.charAt(2));
 		// 6. BONUS -- print the LAST 3 char's of your string using
-		//        .length() to determine WHERE the last 3 char's are located.
+		// .length() to determine WHERE the last 3 char's are located.
+
+		System.out.println(C.length() - 3, C.length());
+
 	}
 }
-
