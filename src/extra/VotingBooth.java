843e49735572d0803f31969b180dc7ab6c418e40
jadenwalker01 <league@WorthyMini-6.attlocal.net>
2017-12-23 12:04:39 -0800

    Voting Boot

--- src/extra/VotingBooth.java
+++ src/extra/VotingBooth.java
@@ -1,5 +1,17 @@
 package extra;
 
-public class VotingBooth {
+import javax.swing.JOptionPane;
 
+public class VotingBooth {
+public static void main(String[] args) {
+	
+	String Answer = JOptionPane.showInputDialog("How old are you?");
+	int age = Integer.parseInt(Answer);
+	if(age > 18) {
+		String answer = JOptionPane.showInputDialog("Who the next president should be?");
+		System.out.println(answer);
+	}else {
+		JOptionPane.showMessageDialog(null, "Nobody cares what you think about who the next president should be!");
+	}
+}
 }
